3fc475f56abd2753458934e6cc00292cfa609779
sho <shocho0101@hotmail.co.jp>
2018-04-13 19:29:50 +0900

    完成

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func natsuPlayButton(){
+        setAudioPlayer(soundName: "natsu", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func amePlay(){
+        setAudioPlayer(soundName: "ame", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func akiPlayButton(){
+        setAudioPlayer(soundName: "aki", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func stop(){
+        audioPlayer.stop()
+    }
+    
+    func setAudioPlayer(soundName: String, type: String){
+        let soundFilePath = Bundle.main.path(forResource: soundName, ofType: type)
+        let fileURL = URL(fileURLWithPath: soundFilePath!)
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+        }catch{
+            print("音楽ファイルが読み込めませんでした")
+        }
+    }
 
 }
 
